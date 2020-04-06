cb8bb454ac47263a266e9632653a7e5977515a62
Roberto <roberto@Robertos-iMac.local>
2020-04-05 22:10:37 -0400

    Create Ongs

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost'
+    origin: 'http://localhost:3000'
 }))
 app.use(express.json())
 
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -19,7 +19,7 @@ export default function Profile(){
                 </button>
             </header>
 
-            <h1>Cados cadastrados</h1>
+            <h1>Casos cadastrados</h1>
 
             <ul>
                 <li>
@@ -31,6 +31,51 @@ export default function Profile(){
 
                     <strong>VALOR:</strong>
                     <p>R$ 120,00</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
                 </li>
             </ul>
         </div>
